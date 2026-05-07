daa2c13b107a8ba05dd61fcdfe02213a8979f866
sarveshmarne <sarvesh.14.marne@gmail.com>
2026-05-07 17:52:52 +0530

    .

---
+++ test_final.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+current_df = pd.read_csv('data/raw/movies_2025_complete.csv')
+missing_movies = ['Sunny Sanskari Ki Tulsi Kumari', 'Badass Ravi Kumar']
+
+for movie in missing_movies:
+    found = movie in current_df.Name.values
+    print(f'{movie}: {" FOUND" if found else " NOT FOUND"}')
+
+print(f'\nTotal movies: {len(current_df)}')
+print(f'All movies: {sorted(current_df.Name.tolist())}')
